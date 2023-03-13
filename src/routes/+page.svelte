de0c66619f7e1c324f677419f6528e5213da1455
David Klemme <david@berlinersoftwareschmiede.de>
2023-03-13 21:55:46 +0100

    add sceleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style global lang="postcss">
+	@tailwind base; @tailwind components; @tailwind utilities;
+</style>
+
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+    console.log(data)
+</script>
+
+
+<html lang="en" data-theme="pastel">
+
+<div class="flex flex-col items-center justify-center w-full">
+    <h1 class="text-xl font-extrabold font-sans m-8">Welcher Käse passt zu meinem Wein?</h1>
+    <div class="form-control w-full max-w-md rounded-sm">
+        <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+      </div>
+     
+      {#await data}
+        <p class="m-12 text-lg font-thin">{data.content}</p>
+      {/await}
+    </div>
+</html>
+
